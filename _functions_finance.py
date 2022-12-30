8e906a893a048f5f58f52d121d6a1ed40a4da311
Kaustubh R. Mote <kaustubh.mote@gmail.com>
2022-12-30 07:06:45 +0530

    added compound interest in years for convenience

--- _functions_finance.py
+++ _functions_finance.py
@@ -22,6 +22,27 @@ def compound_interest(starting_amount, time_blocks, interest_per_block):
     return starting_amount * (interest ** time_blocks)
 
 
+def compound_interest_years(starting_amount, years, annual_interest_percent_rate):
+    """
+    Coumpound interest calculator
+    [ipycalc entry point]
+
+    Parameters
+    ----------
+    starting_amount : float
+        initial amount
+    years : int
+        number of years
+    annual_interest_percent_rate : float
+        percentage interest 
+    """
+
+    time_blocks = years * 12
+    interest = 1 + annual_interest_rate / 1200
+
+    return compound_interest(starting_amount, time_blocks, interest_per_block)
+
+
 def sip(seed=0, investment_per_time=0, time_blocks=0, interest_per_block=0):
     """
     _summary_
